b28faf5b45ba4dbbaf082d98523426dc8e103fec
taufikmuharrom <taufikmuharromdev@gmail.com>
2023-10-12 14:22:09 +0700

    setup project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="font-bold flex justify-center">HELLO WORLD</div>
+    </>
+  );
+}
+
+export default App;
